c2c8c5d4c6424e6d91f298508a7fe0a7cc2438eb
santoshikalaskar <kalaskars1996@gmail.com>
2020-02-19 12:19:54 +0530

    [santoshi].Refactor.added resizing of profile picture.

--- Fundooo/Loginregistration/models.py
+++ Fundooo/Loginregistration/models.py
@@ -10,6 +10,7 @@
 from django.db import models
 from django import forms
 from django.contrib.auth.models import User
+from PIL import Image
  
 class Registration(models.Model):
     
@@ -28,3 +29,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self,*args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+        print(img)
+        (width, height) = img.size
+        if height > 200 or width > 200:
+            output_size = (200, 200)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
